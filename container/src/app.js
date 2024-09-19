4bc18a5fffc4ae92397b1c03a7774ef39c142dc1
tmohammad78 <mohammadtaheri506@gmail.com>
2024-09-19 12:47:35 +0330

    add routing

--- container/src/app.js
+++ container/src/app.js
@@ -1,11 +1,22 @@
+import Header from "./components/Header"
 import MarketingApp from "./components/MarketingApp"
 import React from "react"
+import { BrowserRouter } from "react-router-dom"
+import { StylesProvider, createGenerateClassName } from "@material-ui/core/styles"
+
+const generateClassName = createGenerateClassName({
+    productionPrefix: 'co'
+})
 
 export default () => {
     return (
-        <div>
-            this is Container app.
-            <MarketingApp />
-        </div>
+        <BrowserRouter>
+            <StylesProvider generateClassName={generateClassName}>
+                <div>
+                    <Header />
+                    <MarketingApp />
+                </div>
+            </StylesProvider>
+        </BrowserRouter>
     )
 }
